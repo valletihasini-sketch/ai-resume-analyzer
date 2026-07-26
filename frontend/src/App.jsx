e94df7150ac567c25a39b8907476775b26f6f107
Hasini Valleti <valletihasini@gmail.com>
2026-07-26 23:05:27 +0530

    Create react frintend homepage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>AI Resume Analyzer</h1>
+
+      <p>Upload your resume and get an ATS score.</p>
+
+      <button>Upload Resume</button>
+
+      <p>No file selected</p>
+    </div>
+  );
+}
+
+export default App;

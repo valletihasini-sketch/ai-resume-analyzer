22b5d2d15896a38ecde5af38dc2b69df56e177e0
Hasini Valleti <valletihasini@gmail.com>
2026-08-02 19:27:12 +0530

    Improve resume upload UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
   return (
     <div className="container">
       <h1>AI Resume Analyzer</h1>
 
       <p>Upload your resume and get an ATS score.</p>
 
-      <button>Upload Resume</button>
+      <>
+        <input
+          type="file"
+          id="resumeInput"
+          accept=".pdf"
+          style={{ display: "none" }}
+          onChange={(event) => {
+            setSelectedFile(event.target.files[0]);
+          }}
+        />
 
-      <p>No file selected</p>
+        <button onClick={() => document.getElementById("resumeInput").click()}>
+          Upload Resume
+        </button>
+      </>
+
+      {selectedFile ? (
+  <>
+    <p>📄 {selectedFile.name}</p>
+    <p>Size: {(selectedFile.size / 1024).toFixed(2)} KB</p>
+    <button
+  onClick={() => {
+    setSelectedFile(null);
+  }}
+>
+  Remove File
+</button>
+  </>
+) : (
+  <p>No file selected</p>
+)}
     </div>
   );
 }

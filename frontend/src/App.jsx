d06c9fde122f258b520e2532929c0c48dd10eaf8
Hasini Valleti <valletihasini@gmail.com>
2026-08-03 23:56:12 +0530

    Add PDF validation and resume text input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
 
   const [selectedFile, setSelectedFile] = useState(null);
+  const [error, setError] = useState("");
+  const [resumeText,setResumeText]=useState("");
 
   return (
     <div className="container">
@@ -13,19 +15,50 @@ function App() {
 
       <>
         <input
-          type="file"
-          id="resumeInput"
-          accept=".pdf"
-          style={{ display: "none" }}
-          onChange={(event) => {
-            setSelectedFile(event.target.files[0]);
-          }}
-        />
+  type="file"
+  id="resumeInput"
+  accept=".pdf"
+  style={{ display: "none" }}
+  onChange={(event) => {
+
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    if (file.type !== "application/pdf") {
+      setError("Please upload a PDF file.");
+      setSelectedFile(null);
+      return;
+    }
+
+    setError("");
+    setSelectedFile(file);
+  }}
+/>
 
         <button onClick={() => document.getElementById("resumeInput").click()}>
           Upload Resume
         </button>
       </>
+      <hr />
+
+<h2>Or Paste Resume</h2>
+
+<textarea
+  className="resume-text"
+  placeholder="Paste your resume here..."
+  value={resumeText}
+  onChange={(event) => setResumeText(event.target.value)}
+></textarea>
+
+<button className="analyze-btn">
+  Analyze Resume
+</button>
+      {error && (
+  <p style={{ color: "red" }}>
+    {error}
+  </p>
+)}
 
       {selectedFile ? (
   <>
@@ -34,6 +67,7 @@ function App() {
     <button
   onClick={() => {
     setSelectedFile(null);
+    setError("");
   }}
 >
   Remove File
